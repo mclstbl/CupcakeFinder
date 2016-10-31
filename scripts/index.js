5eef4be0d0ecb4848ba02691a9fb24d0abd97771
Micaela Estabillo <micaela.estabillo@gmail.com>
2016-10-31 11:22:22 -0400

    Use HTML5 geolocation API to get user's location

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+// This file is based on an example given in class. Some changes have
+// adapted to satisfy the needs of DonationBoxFinder.
+
+// This function uses the HTML5 Geolocation API to get a user's location.
+function getLocation() {
+    if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        document.getElementById("status").innerHTML="Geolocation is not supported by this browser.";
+    }
+}
+
+// This function displays the user's position in the input box
+// for location.
+function showPosition(position) {
+    document.getElementById("location").placeholder = "";
+    document.getElementById("location").value = position.coords.latitude + ", " + position.coords.longitude;
+
+    // Display on a map
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
+    //document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
+
+// This function displays messages that explain errors in using the
+// geolocation API.
+function showError(error) {
+    var msg = "";
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            msg = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            msg = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            msg = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            msg = "An unknown error occurred."
+            break;
+    }
+    document.getElementById("status").innerHTML = msg;
+}
