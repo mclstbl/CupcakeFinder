694c33909d4c1f7e967565974759356464b419d9
EC2 Default User <ec2-user@ip-172-31-19-116.us-west-2.compute.internal>
2016-12-05 23:17:33 +0000

    Fix css link

--- public/individual.php
+++ public/individual.php
@@ -8,13 +8,13 @@ function generateHeader() {
     . show_title(getName($_GET['place_id']))
     . '
         <link rel="stylesheet" href="' 
-    . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/public/css/style.css'
+    . 'https://' . $_SERVER["HTTP_HOST"] . '/public/css/style.css'
     .'">
         <link rel="icon" href="'
-    . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/public/images/logo.png'
+    . 'https://' . $_SERVER["HTTP_HOST"] . '/public/images/logo.png'
     . '">
         <script type ="text/javascript" src="'
-    . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/scripts/individual.js'
+    . 'https://' . $_SERVER["HTTP_HOST"] . '/scripts/individual.js'
     . '"></script>
         <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA10vc2deGv18oPyOA1w1k7H6i7mAIzMuA" type="text/javascript"></script>
     </head>
@@ -22,24 +22,24 @@ function generateHeader() {
         <div id="header-container">
             <div id="header-container">
             <h1 id="headertitle"><a href='
-            . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/index.php'
+            . 'https://' . $_SERVER["HTTP_HOST"] . '/index.php'
             .' >Donation Box Finder</a></h1>
             <h2 id="headersubtitle">Find donation centres near you</h2>
             <div id="nav"><ul>
                 <li><a class="menubutton" href="'
-            . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/index.php'
+            . 'https://' . $_SERVER["HTTP_HOST"] . '/index.php'
             .'">Home</a></li>
                 <li><a class="menubutton" id="active" href="'
-            . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/public/search.php'
+            . 'https://' . $_SERVER["HTTP_HOST"] . '/public/search.php'
             .'">Search</a></li>
                 <li><a class="menubutton" href="'
-            . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/public/submission.php'
+            . 'https://' . $_SERVER["HTTP_HOST"] . '/public/submission.php'
             .'">Submit</a></li>
                 <li><a class="menubutton" href="'
-            . 'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/public/profile.php'
+            . 'https://' . $_SERVER["HTTP_HOST"] . '/public/profile.php'
             .'">Profile</a></li>
                 <li><a class="menubutton" href="'
-            .  'https://' . $_SERVER["HTTP_HOST"] . '/DonationBoxFinder/scripts/logout.php'
+            .  'https://' . $_SERVER["HTTP_HOST"] . '/scripts/logout.php'
             .'">Logout</a></li>';
 }
 ?>
