9e66fd1b06f150bc4d096fe80b5d513ea2f52aa0
Micaela Estabillo <micaela.estabillo@gmail.com>
2016-12-05 15:01:39 -0500

    Rename results.php to search.php

---
+++ public/search.php
@@ -0,0 +1,74 @@
+<?php
+require_once "../scripts/shared.php";
+require_once "../scripts/menu.php";
+
+// Default values always return something because they search for the minimum criteria.
+$clothing = $electronics = $food = false;
+$stars = 1;
+$latitude = 0;
+$longitude = 0;
+
+$results = getPlaces($_POST);
+
+//echo ($clothing . ' ' . $electronics . ' ' . $food . ' ' . $stars . ' ' . $latitude . ' ' . $longitude . ' ');
+?>
+<!DOCTYPE php>
+<php lang="en">
+    <head>
+        <?php
+            show_title("Search results");
+        ?>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="icon" href="images/logo.png">
+        <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA10vc2deGv18oPyOA1w1k7H6i7mAIzMuA" type="text/javascript"
+            locations="<?php echo json_encode(htmlentities($results));?>"></script>
+        <script type ="text/javascript" src="../scripts/results.js"></script>
+    </head>
+    <body onload="loadMap()">
+        <div id="header-container">
+<!-- The menu.php show_header function is used to generate the nav bar.
+Passing the page title determines what the generated header looks like. -->
+            <?php
+                show_header("Search");
+            ?>
+        </div>
+        
+        <div id="content-container">
+            <div id="results-content">
+                <!--div id="mini-search">
+                    <form id="search" action="#">
+                        <legend>Donate </legend>
+                        <fieldset>
+                        <input id="clothing" type="checkbox" value="clothing"><label for="clothing"> Clothing</label>
+                        <input id="electronics" type="checkbox" value="electronics"><label for="electronics"> Electronics</label>
+                        <input id="food" type="checkbox" value="food"><label for="food"> Food</label><br>
+                        </fieldset>
+
+                        <label for="near">Near</label>
+                        <input id="near" type="text" name="location" placeholder="My location">
+                        <input type="submit" value="Search">
+                    </form>
+                </div-->
+                <a name="results">
+                <h3>Search results</h3>
+
+                <div id="map">
+                </div>
+                
+                <h4>Scroll down and click name for details</h4>
+
+                <div id="list">
+                    <ol class="hits">
+                        <?php printSearchResults($results); ?>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer-container">
+            <footer>
+                Micaela Estabillo's CS 4WW3 Project
+            </footer>
+        </div>
+    </body>
+</php>
