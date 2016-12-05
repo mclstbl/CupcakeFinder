66fc7164408055b0b56022e2bc50c6cff8d1eb88
Micaela Estabillo <micaela.estabillo@gmail.com>
2016-12-05 17:31:52 -0500

    Refactoring

--- public/individual.php
+++ public/individual.php
@@ -1,6 +1,88 @@
 <?php
 require_once "../scripts/shared.php";
 require_once "../scripts/menu.php";
+
+// Prints user reviews in individual pages.
+function generateReviews($p_id) {
+    $db = getDB();
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    error_reporting(E_ALL);
+
+// Prepare the select query.
+    $query = $db->prepare('SELECT * FROM reviews where place_id=:place_id');
+// Execute the query using passed in values as parameters.
+    $query->execute(array('place_id'=>$p_id));
+    $info = $query->fetchAll();
+
+    echo '<div id="reviewpane">';
+    foreach ($info as $row) {
+        echo '<div class="review">
+                <div class="user">
+                    <img src='
+            . $row["photo"]
+            . 'alt="User photo">
+                    <h4>' 
+            . $row["username"] 
+            . '</h4>
+                </div>
+                <div class="star-rating">'
+            . printStars($row['stars'])
+            . '</div>
+                <div class="comment">'
+            . $row["review"]
+            .'
+                </div>
+            </div>';
+    }
+    echo '</div>';
+}
+
+// Gets the place's name for title printing.
+function getName($p_id) {
+    $db = getDB();
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    error_reporting(E_ALL);
+
+// Prepare the select query.
+    $query = $db->prepare('SELECT * FROM places where place_id=:place_id');
+// Execute the query using passed in values as parameters.
+    $query->execute(array('place_id'=>$p_id));
+    $info = $query->fetchAll();
+
+    return $info[0]['placename'];
+}
+
+// Generates individual pages.
+function generateIndividual($p_id) {
+    $db = getDB();
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    error_reporting(E_ALL);
+
+// Prepare the select query.
+    $query = $db->prepare('SELECT * FROM places where place_id=:place_id');
+// Execute the query using passed in values as parameters.
+    $query->execute(array('place_id'=>$p_id));
+    $info = $query->fetchAll();
+
+    print_r($info);
+    echo '<h3 id="placename">'
+    . $info[0]["placename"]
+    . ' </h3>
+        <h4>'
+    .       $info[0]["address"]
+    . ' </h4>
+    <div id="productpane">
+        <div id="description">'
+            . $info[0]["description"]
+    .   '</div>'
+    .   '<div id="pictures"><img id="pic" src="'
+        . $info[0]["photo"]
+        . '" alt="Photo of location">
+        </div>
+        <div id="map">
+        </div>
+    </div>';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- public/submission.php
+++ public/submission.php
@@ -5,6 +5,132 @@ require("../scripts/S3.php");
 
 $placenameError = $latitudeError = $longitudeError = "";
 
+// This function uploads a submitted file to the dbf-photos S3 bucket.
+function uploadToS3($file) {
+// Check if the file is actually a photo.
+// tmp_name is what PHP temporarily names the photo file.
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    if ($finfo->file($_FILES['newphoto']['tmp_name']) === "image/jpeg") {
+        $fileextension = "jpg";
+    }
+// Cancel upload if its type is not jpg.
+    else {
+        echo 'Uploaded file was not a valid image.';
+        return;
+    }
+// Use the file's SHA1 hash as filename in order to make it unique.
+    $filehash = sha1_file($_FILES['newphoto']['tmp_name']);
+    $filename = $filehash . "." . $fileextension;
+// Get AWS S3 credentials from credentials.php.
+    $awsAccessKey = getS3AccessId();
+    $awsSecretKey = getS3SecretAccessKey();
+    $bucketName = getS3BucketName();
+// Use S3.php to create a connection to the bucket.
+    $s3 = new S3($awsAccessKey, $awsSecretKey);
+// Try to put the photo in the bucket.
+    $ok = $s3->putObjectFile($_FILES['newphoto']['tmp_name'], $bucketName, $filename, S3::ACL_PUBLIC_READ);
+// Check for success and update file URL which is returned.
+    if ($ok) {
+        $url = 'http://' . $bucketName . '.s3-website-us-west-2.amazonaws.com/' . $filename;
+        return $url;
+    }
+    return "";
+}
+
+// This function adds a new place to the database. It is used in the submission page.
+function addPlace($method, $photourl) {
+// Initialize variables to hold image information.
+    $place_id = -1;
+    $placename = $description = "";
+    $address = "See coordinates";
+    $clothing = $electronics = $food = (bool) 0;
+    $latitude = $longitude = 0;
+// Loop through POST values and assign them to their respective variables.
+    foreach ($method as $key=>$data) {
+        global $place_id, $placename, $description, $address, $clothing, $electronics, $food;
+        global $latitude, $longitude;
+        global $placenameError, $latitudeError, $longitudeError;
+// Only the place's name and location (longitude and latitude) are required. Everything else is optional.
+        switch ($key) {
+            case "placename":
+                if(isset($method[$key]) and $method[$key] != '') {
+                    $placename = $data;
+                }
+                else
+                {
+                    $placeNameError = "Name is required";
+                    return false;
+                }
+                break;
+            case "description":
+                if(isset($method[$key])) {
+                    $description = $data;
+                }
+                break;
+            case "address":
+                if(isset($method[$key])) {
+                    $address = $data;
+                }
+                break;
+            case "latitude":
+                if(isset($method[$key]) and $method[$key] != '') {
+                    $latitude = $data;
+                }
+                else {
+                    $latitudeError = "Latitude is required";
+                    return false;
+                }
+                break;
+            case "longitude":
+                if(isset($method[$key]) and $method[$key] != '') {
+                    $longitude = $data;
+                }
+                else {
+                    $longitude = "Longitude is required";
+                    return false;
+                }
+                break;
+            case "clothing":
+                if(isset($method[$key])) {
+                    $clothing = (bool) 1;
+                }
+                break;
+            case "electronics":
+                if(isset($method[$key])) {
+                    $electronics = (bool) 1;
+                }
+                break;
+            case "food":
+                if(isset($method[$key])) {
+                    $food = (bool) 1;
+                }
+                break;
+        }
+    }
+// Use PDO to connect to the database.
+// Do not catch to reveal DB errors.
+    $db = getDB();
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    error_reporting(E_ALL);
+// Get the new place_id by incrementing current number of rows.
+    $place_id = $db->query('SELECT * FROM places')->rowCount() + 1;
+// Return if place_id failed to be updated.
+    if ($place_id < 1) return;
+// Debug print
+// echo ($place_id . $placename . $description . $address . $clothing . $electronics . $food . $latitude . $longitude);
+// Prepare the insert query.
+    $query = $db->prepare("
+        INSERT INTO places (place_id, placename, address, description, clothing, electronics, food, latitude, longitude, photo)
+        VALUES(:place_id, :placename, :address, :description, :clothing, :electronics, :food, :latitude, :longitude, :photo)
+    ");
+// Execute the query using passed in values as parameters.
+    return($query->execute(array('place_id'=>$place_id, 'placename'=>$placename,
+        'address'=>$address, 'description'=>$description, 'clothing'=>$clothing,
+        'electronics'=>$electronics, 'food'=>$food, 'latitude'=>$latitude,
+        'longitude'=>$longitude, 'photo'=>$photourl)));
+    return false;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" and !empty($_POST)) {
     $photourl = "";
     if (file_exists($_FILES['newphoto']['tmp_name'])) {
